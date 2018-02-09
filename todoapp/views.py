ce437b4ab5ad348512b1ecebaa290363f2fb7f30
seanthaswan <sean-swan@hotmail.com>
2018-02-09 15:10:40 -0800

    Finished code along for Django proficiency

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,26 +3,77 @@ from django.contrib import auth
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+import requests
+import json
 
 # Main routes
 def index(request):
     if request.method == "GET":
-        return HttpResponse('index GET')
+      todos = Todo.objects.all().order_by('text')
+      users = User.objects.all()
+      r = requests.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=')
+      quote = json.loads(r.text)
+      return render(request, 'todoapp/index.html', {
+        'todos': todos,
+        'users': users,
+        'quote': quote[0]
+         })
     elif request.method == "POST":
-        return HttpResponse('index POST')
+      try:
+        userid = request.POST['userid']
+      except (KeyError):
+        return render(request, 'todoapp/index.html', {'error': 'You must select a user', 'users' : users, 'todos': todo})
+      else:
+        new_todo = Todo()
+        new_todo.text = request.POST['text']
+        new_todo.user = User.objects.get(pk=userid)
+        new_todo.save()
+        return redirect('index')
+
 
 def delete(request, todo_id):
-    return HttpResponse("Delete this")
+    item = Todo.objects.get(id=todo_id)
+    item.delete()
+    return redirect('index')
 
-def delete(request, todo_id):
-    return HttpResponse("Mark done")
+def done(request, todo_id):
+    item = Todo.objects.get(id=todo_id)
+    item.is_complete = True
+    item.save()
+    return redirect('index')
 
 # Auth-related routes
 def signup(request):
-    return HttpResponse('signup')
+    if request.method == 'GET':
+      return render(request, 'todoapp/signup.html')
+    elif request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      firstname = request.POST['firstname']
+      lastname = request.POST['lastname']
+      email = request.POST['email']
+      try:
+        user = User.objects.create_user(username=username, password=password, first_name=firstname, last_name=lastname, email=email)
+        if user is not None:
+          return login(request)
+      except:
+        return render(request, 'todoapp/signup.html', {'error': 'Username or Email already exists'})
+
 
 def login(request):
-    return HttpResponse('login')
+    if request.method == 'GET':
+      return render(request, 'todoapp/login.html')
+    elif request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      user = auth.authenticate(username=username, password=password)
+      if user is not None:
+        auth.login(request, user)
+        return redirect('index')
+      else:
+        return render(request, 'todoapp/login.html', {'error': 'Invalid Credentials'})
+
 
 def logout(request):
-    return HttpResponse('logout')
+    auth.logout(request)
+    return redirect('index')
